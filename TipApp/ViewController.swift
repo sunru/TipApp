83531fd852474697e694a3c294477f721d7d7def
RubySun <sunru10075@gmail.com>
2022-12-06 18:51:21 +0800

    Initial Commit

---
+++ TipApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipApp
+//
+//  Created by 廖晨如 on 2022/12/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
